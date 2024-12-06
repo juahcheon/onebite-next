ac6a461aa48f5226724023f812184d7303c54ccd
juah <juacheon@gmail.com>
2024-12-06 22:07:33 +0900

    .

--- section05/src/app/(with-searchbar)/page.tsx
+++ section05/src/app/(with-searchbar)/page.tsx
@@ -1,10 +1,11 @@
 import BookItem from "../../components/book-item";
 import style from "./page.module.css";
 import { BookData } from "../../types";
-
-export const dynamic = 'force-dynamic';
+import { delay } from "../util/delay";
+import { Suspense } from "react";
 
 async function AllBooks() {
+  await delay(1500);
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`, 
     { cache: "force-cache" });
@@ -23,6 +24,7 @@ async function AllBooks() {
 }
 
 async function RecoBooks() {
+  await delay(3000);
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`, 
     { next: { revalidate: 3 } });
@@ -39,16 +41,22 @@ async function RecoBooks() {
   </div>
 }
 
+export const dynamic = "force-dynamic";
+
 export default function Home() {
   return (
     <div className={style.container}>
       <section>
         <h3>지금 추천하는 도서</h3>
-        <RecoBooks />
+        <Suspense fallback={<div>도서를 불러오는 중입니다...</div>}>
+          <RecoBooks />
+        </Suspense>
       </section>
       <section>
         <h3>등록된 모든 도서</h3>
-        <AllBooks />
+        <Suspense fallback={<div>도서를 불러오는 중입니다...</div>}>
+          <AllBooks />
+        </Suspense>
       </section>
     </div>
   );
--- section05/src/app/(with-searchbar)/search/page.tsx
+++ section05/src/app/(with-searchbar)/search/page.tsx
@@ -1,12 +1,10 @@
+import { Suspense } from "react";
 import BookItem from "../../../components/book-item";
 import { BookData } from "../../../types";
+import { delay } from "../../util/delay";
 
-export default async function Page({
-  searchParams,
-}: {
-  searchParams: Promise<{ q: string }>;
-}) {
-  const { q } = await searchParams;
+async function SearchResult({ q }: { q: string }) {
+  await delay(2000);
 
   const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${q}`, { cache: "force-cache" });
   if(!response.ok) {
@@ -21,5 +19,19 @@ export default async function Page({
         <BookItem key={book.id} {...book} />
       ))}
     </div>
+  );
+}
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: Promise<{ q?: string }>;
+}) {
+  const { q } = await searchParams;
+
+  return (
+    <Suspense key={q || ""} fallback={<div>Loading...</div>}>
+      <SearchResult q={q || ""} />
+    </Suspense>
   );
 }
