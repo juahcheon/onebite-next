bf2ed3b012a7fb4efd09b67dc8a893b946ef620b
juah <juacheon@gmail.com>
2024-12-08 20:22:26 +0900

    .

--- section05/src/app/(with-searchbar)/search/page.tsx
+++ section05/src/app/(with-searchbar)/search/page.tsx
@@ -2,6 +2,7 @@ import { Suspense } from "react";
 import BookItem from "../../../components/book-item";
 import { BookData } from "../../../types";
 import { delay } from "../../util/delay";
+import BookListSkeleton from "section05/src/components/skeleton/book-list-skeleton";
 
 async function SearchResult({ q }: { q: string }) {
   await delay(2000);
@@ -30,7 +31,7 @@ export default async function Page({
   const { q } = await searchParams;
 
   return (
-    <Suspense key={q || ""} fallback={<div>Loading...</div>}>
+    <Suspense key={q || ""} fallback={<BookListSkeleton count={3} />}>
       <SearchResult q={q || ""} />
     </Suspense>
   );
